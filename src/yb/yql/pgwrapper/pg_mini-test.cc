fe001bc7fe346f32818e00d3d7b8e18b26e9ceaa
Jason Kim <jj-kim@users.noreply.github.com>
2019-12-11 15:08:51 -0800

    Convert assertions on pointer change to comment

--- src/yb/yql/pgwrapper/pg_mini-test.cc
+++ src/yb/yql/pgwrapper/pg_mini-test.cc
@@ -749,9 +749,8 @@ TEST_F(PgMiniTest, YB_DISABLE_TEST_IN_TSAN(DropDBMarkDeleted)) {
   std::this_thread::sleep_for(kSleepTime);
   ASSERT_FALSE(catalog_manager->AreTablesDeleting());
   // Make sure that the table deletions are persisted.
-  ASSERT_EQ(catalog_manager, cluster_->leader_mini_master()->master()->catalog_manager());
   ASSERT_OK(cluster_->RestartSync());
-  ASSERT_NE(catalog_manager, cluster_->leader_mini_master()->master()->catalog_manager());
+  // Refresh local variable after RestartSync.
   catalog_manager = cluster_->leader_mini_master()->master()->catalog_manager();
   ASSERT_FALSE(catalog_manager->AreTablesDeleting());
 }
